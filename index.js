bd6cba46fb28eacb17f7d2e068bc5c04122a56b4
Luiz Nascimento <luizfelipesilvadonascimento@gmail.com>
2021-04-08 16:04:13 -0300

    creating html content

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = 5014 || process.env.PORT 
+const rootDirectory = path.join(__dirname, '/public')
+
+app.use(express.static(rootDirectory))
+
+app.listen(port, () => {
+    console.log('listening up to ', port)
+})
